b3ae006a619ccdb0651431d2267ce900392bb612
Felipe Matthew Moreira Nascimento <102431464+FelipeMatthew@users.noreply.github.com>
2022-11-10 02:17:52 -0300

    Create main.js

---
+++ Seção37_FinalProject/js/main.js
@@ -0,0 +1,28 @@
+(function(){
+
+    var $body = document.querySelector('body');
+    $body.classList.remove('no-js')
+    $body.classList.add('js')
+    
+        
+    var menu = new Menu({
+        container: '.header__nav',
+        toggleBtn: '.header__btnMenu',
+        widthEnabled: 1024 
+    })
+
+    var carouselImgs = new Carousel({
+        container: '.laptop-slider .slide-show',
+        itens: 'figure',
+        btnPrev: '.prev',
+        btnNext: '.next'
+    })
+
+    var carouselQuotes = new Carousel({
+        container: '.quote_slideshow',
+        itens: 'figure',
+        btnPrev: '.prev',
+        btnNext: '.next'
+    })
+
+})()
